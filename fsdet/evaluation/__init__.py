fd35ad0a11507700c3e35e9e21e7855b1a7e823b
amajee11us <anaymajee11us@gmail.com>
2024-03-08 20:29:35 -0600

    Removed existing files without clearing history

--- fsdet/evaluation/__init__.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
-from .coco_evaluation import COCOEvaluator
-from .evaluator import DatasetEvaluator, DatasetEvaluators, inference_context, inference_on_dataset
-from .lvis_evaluation import LVISEvaluator
-from .pascal_voc_evaluation import PascalVOCDetectionEvaluator
-from .idd_detection_evaluation import IDDDetectionEvaluator
-from .testing import print_csv_format, verify_results
-
-__all__ = [k for k in globals().keys() if not k.startswith("_")]
--- scripts/automate.sh
+++
@@ -1,6 +0,0 @@
-declare -a seeds=("1" "2" "3" "4" "5" "6" "7" "8" "9" "10")
-for s in "${seeds[@]}"
-do
-    sed -i "s/seed[0-9]/seed"$s"/g" configs/PASCAL_VOC/split1/10shot_CL_IoU.yml
-    python tools/train_net.py --config-file configs/PASCAL_VOC/split1/10shot_CL_IoU.yml
-done
