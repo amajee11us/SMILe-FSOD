4582ab7bdb37c5b74fc0e114c14ba8775fbae7ca
amajee11us <anay.majee@utdallas.edu>
2024-03-11 15:42:12 -0500

    Runner code for combinatorial objectives

--- tools/dist_train.sh
+++ tools/dist_train.sh
@@ -2,8 +2,15 @@
 
 CONFIG=$1
 GPUS=$2
-PORT=${PORT:-29500}
+ROI_LOSS=$3
+PORT=${PORT:-29501}
+
+if [[ -z "$ROI_LOSS" ]]; then
+    export ROI_LOSS_TYPE="SupCon"
+else
+    export ROI_LOSS_TYPE=$3
+fi
 
 PYTHONPATH="$(dirname $0)/..":$PYTHONPATH \
 python -m torch.distributed.launch --nproc_per_node=$GPUS --master_port=$PORT \
-    $(dirname "$0")/train.py $CONFIG --launcher pytorch ${@:3}
+    $(dirname "$0")/train.py $CONFIG --launcher pytorch ${@:4}
